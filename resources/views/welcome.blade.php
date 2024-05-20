44bb5015fc6bc8f5e7dd6f72cdb7a19f2974d014
Your Name <you@example.com>
2024-05-20 11:34:30 +0900

    라인2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
         </style>
     </head>
     nice goods
+    line 2
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
